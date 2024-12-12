87fad2988ba198d734a4b65a87e2d83308da73d5
Екатерина Балашова <rinaleem@mail.ru>
2024-12-12 18:10:54 +0400

    Updated

--- Maze/Forms/Gamer.Designer.cs
+++ Maze/Forms/Gamer.Designer.cs
@@ -316,7 +316,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(620, 36);
             this.label4.TabIndex = 36;
-            this.label4.Text = "0            1             2             3             4";
+            this.label4.Text = "0               1                2                3              4";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.label4.Visible = false;
             // 
